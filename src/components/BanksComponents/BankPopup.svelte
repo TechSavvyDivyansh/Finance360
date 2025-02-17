430f0720352892c8394bc4f9eb9dcab3a4b73c86
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2025-02-17 15:36:28 +0530

    popup partially done

--- src/components/BanksComponents/BankPopup.svelte
+++ src/components/BanksComponents/BankPopup.svelte
@@ -1,4 +1,5 @@
 <script>
+	import FormInput from '../FormInput.svelte';
     import hdfc from '/src/assets/HDFC-Bank-Limited-Symbol.png'
     let banks=[
         {
@@ -21,6 +22,10 @@
             name:"SBI",
             logo:"/src/assets/SBI-Logo.png"
         },
+        {
+            name:"Custom Bank",
+            logo:"/src/assets/custom-bank.png"
+        }
     ]
 
 </script>
@@ -29,12 +34,25 @@
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 
 <div class="absolute top-0 left-0 w-[100vw] h-[100vh] bg-[#00000091] backdrop-blur-sm flex items-center justify-center cursor-pointer"  on:click|self>
-    <div class="h-[80vh] w-[35vw] bg-[#1a1a1a] shadow-2xl rounded-xl cursor-default p-10 overflow-hidden">
+    <div class="h-[80vh] w-[35vw] bg-[#1a1a1a] shadow-2xl rounded-xl cursor-default p-10 overflow-hidden relative">
         <p class="text-[#a8a8a8] text-xl">Select your bank</p>
         <div class="flex py-8 gap-12 overflow-x-scroll custom-scrollbar">
             {#each banks as bank}
                 <img src={bank.logo} alt={bank.name} class="w-[150px] h-[100px] object-cover rounded-lg cursor-pointer bg-[#3a3a3a] p-2">
             {/each}
         </div>
+
+
+        <div class="my-10 flex flex-col gap-7">
+            <FormInput
+                placeholder="Bank Name"
+            />
+            <FormInput
+                placeholder="Account Number"
+                label="Account Number"
+            />
+            
+        </div>
+        <button class="bg-gradient-to-br from-[#202020] to-[#2f4b3d] cursor-pointer shadow-xl p-5 rounded-xl font-semibold text-xl absolute right-10">Add bank</button>
     </div>
 </div>
